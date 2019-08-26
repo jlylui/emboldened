abe3ec28dca44670d7cddb9231560c75ad5b84fd
duubyPlz <duubyplz@hotmail.com>
2019-08-26 21:45:06 +1000

    auto update

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,102 +1,167 @@
-import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
-const NavBar = props => {
-  const [toggleMenue, setToggleMenue] = useState(false);
-  const [toggleBgColor, setToggleBgColor] = useState(false);
-
-  useEffect(() => {
-    window.addEventListener("scroll", scrollHandler);
-    return () => {
-      window.removeEventListener("scroll", scrollHandler);
+function IndexNavbar() {
+  const [navbarColor, setNavbarColor] = React.useState("navbar-transparent");
+  const [collapseOpen, setCollapseOpen] = React.useState(false);
+  React.useEffect(() => {
+    const updateNavbarColor = () => {
+      if (
+        document.documentElement.scrollTop > 399 ||
+        document.body.scrollTop > 399
+      ) {
+        setNavbarColor("");
+      } else if (
+        document.documentElement.scrollTop < 400 ||
+        document.body.scrollTop < 400
+      ) {
+        setNavbarColor("navbar-transparent");
+      }
+    };
+    window.addEventListener("scroll", updateNavbarColor);
+    return function cleanup() {
+      window.removeEventListener("scroll", updateNavbarColor);
     };
   });
-
-  const scrollHandler = () => {
-    setToggleBgColor(true);
-  };
-  const toggleBarClick = () => {
-    setToggleMenue(!toggleMenue);
-    if (!toggleMenue && !toggleBgColor) setToggleBgColor(true);
-    if (toggleMenue && toggleBgColor) setToggleBgColor(false);
-  };
-
-  const linkClick = () => {
-    if (toggleMenue) {
-      setToggleMenue(!toggleMenue);
-    }
-  };
-
-  const navMenu = props.navMenu.map((menu, index) => {
-    if (index === 0) {
-      return null;
-    } else {
-      return (
-        <li className="nav-item" key={index}>
-          <Link href={menu.link}>
-            <a className="nav-link" onClick={linkClick}>
-              {" "}
-              <span className="nav-top-icon">
-                <FontAwesomeIcon icon={menu.icon} />
-              </span>
-              {menu.label}
-            </a>
-          </Link>
-        </li>
-      );
-    }
-  });
-
-  const dropDownNavMenue = toggleMenue ? (
-    <div className="navbar-collapse d-lg-none">
-      <ul className="navbar-nav d-lg-none">{navMenu}</ul>
-    </div>
-  ) : null;
-
   return (
-    // <nav
-    //   className={
-    //     toggleBgColor
-    //       ? "navbar navbar-fixed-top navbar-expand-lg navbar-light white-bg "
-    //       : "navbar navbar-fixed-top navbar-expand-lg navbar-light bg-transparent "
-    //   }
-    // >
-    //   <div className="container">
-    //     <Link href={props.navMenu[0].link}>
-    //       <a className="navbar-brand">
-    //         <img
-    //           src={`../${props.navMenu[0].logo}`}
-    //           alt=""
-    //           width="30"
-    //           height="30"
-    //         />{" "}
-    //         {props.navMenu[0].brand}
-    //       </a>
-    //     </Link>
-    //     <div
-    //       className="navbar-collapse navbar-fullwidth collapse justify-content-end"
-    //       id="navBarContent"
-    //     >
-    //       <ul className="navbar-nav">{navMenu}</ul>
-    //     </div>
-    //     <a className="navbar-toggler" onClick={toggleBarClick}>
-    //       <FontAwesomeIcon icon="bars" />
-    //     </a>
-    //     {dropDownNavMenue}
-    //   </div>
-    // </nav>
-
-    <nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent " color-on-scroll="400">
-      <div class="container">
-        <div class="navbar-translate">
-          <a class="navbar-brand" data-placement="bottom">
-            asdf
-          </a>
+    <>
+      {collapseOpen ? (
+        <div
+          id="bodyClick"
+          onClick={() => {
+            document.documentElement.classList.toggle("nav-open");
+            setCollapseOpen(false);
+          }}
+        />
+      ) : null}
+      <nav className={"navbar navbar-expand-lg bg-primary fixed-top " + navbarColor} expand="lg" color="info">
+        <div className="container">
+          <div className="navbar-translate">
+            <a className="navbar-brand"
+              href="https://demos.creative-tim.com/now-ui-kit-react/#/index?ref=nukr-index-navbar"
+              target="_blank"
+              id="navbar-brand"
+            >
+              Now UI Kit React
+            </a>
+            <button
+              className="navbar-toggler navbar-toggler"
+              onClick={() => {
+                document.documentElement.classList.toggle("nav-open");
+                setCollapseOpen(!collapseOpen);
+              }}
+              aria-expanded={collapseOpen}
+              type="button"
+            >
+              <span className="navbar-toggler-bar top-bar"></span>
+              <span className="navbar-toggler-bar middle-bar"></span>
+              <span className="navbar-toggler-bar bottom-bar"></span>
+            </button>
+          </div>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            isOpen={collapseOpen}
+            navbar
+          >
+            <ul className="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link"
+                  href="#pablo"
+                  onClick={e => {
+                    e.preventDefault();
+                    document
+                      .getElementById("download-section")
+                      .scrollIntoView();
+                  }}
+                >
+                  <i className="now-ui-icons arrows-1_cloud-download-93"></i>
+                  <p>Download</p>
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav>
+                <DropdownToggle
+                  caret
+                  color="default"
+                  href="#pablo"
+                  nav
+                  onClick={e => e.preventDefault()}
+                >
+                  <i className="now-ui-icons design_app mr-1"></i>
+                  <p>Components</p>
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem to="/index" tag={Link}>
+                    <i className="now-ui-icons business_chart-pie-36 mr-1"></i>
+                    All components
+                  </DropdownItem>
+                  <DropdownItem
+                    href="https://demos.creative-tim.com/now-ui-kit-react/#/documentation/introduction?ref=nukr-index-navbar"
+                    target="_blank"
+                  >
+                    <i className="now-ui-icons design_bullet-list-67 mr-1"></i>
+                    Documentation
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <Button
+                  className="nav-link btn-neutral"
+                  color="info"
+                  href="#pablo"
+                  id="upgrade-to-pro"
+                  onClick={e => e.preventDefault()}
+                >
+                  <i className="now-ui-icons arrows-1_share-66 mr-1"></i>
+                  <p>Upgrade to PRO</p>
+                </Button>
+                <UncontrolledTooltip target="#upgrade-to-pro">
+                  Cooming soon!
+                </UncontrolledTooltip>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  href="https://twitter.com/CreativeTim?ref=creativetim"
+                  target="_blank"
+                  id="twitter-tooltip"
+                >
+                  <i className="fab fa-twitter"></i>
+                  <p className="d-lg-none d-xl-none">Twitter</p>
+                </NavLink>
+                <UncontrolledTooltip target="#twitter-tooltip">
+                  Follow us on Twitter
+                </UncontrolledTooltip>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  href="https://www.facebook.com/CreativeTim?ref=creativetim"
+                  target="_blank"
+                  id="facebook-tooltip"
+                >
+                  <i className="fab fa-facebook-square"></i>
+                  <p className="d-lg-none d-xl-none">Facebook</p>
+                </NavLink>
+                <UncontrolledTooltip target="#facebook-tooltip">
+                  Like us on Facebook
+                </UncontrolledTooltip>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
+                  target="_blank"
+                  id="instagram-tooltip"
+                >
+                  <i className="fab fa-instagram"></i>
+                  <p className="d-lg-none d-xl-none">Instagram</p>
+                </NavLink>
+                <UncontrolledTooltip target="#instagram-tooltip">
+                  Follow us on Instagram
+                </UncontrolledTooltip>
+              </NavItem>
+            </Nav>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
-};
+}
 
-export default NavBar;
+export default IndexNavbar;
