140c79d89e7f55d27374ba0e38c4a0ee6e3d9a64
Joycelyn Lui <joycelyn.lui@hotmail.com>
2020-02-18 23:24:21 +1100

    added team profiles

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Layout from "../components/Layout";
+import ProfileCard from "../components/ProfileCard";
 
 const banner = {
   title: "About us",
@@ -58,24 +59,63 @@ const About = () => {
       <div className="section">
         <div className="container">
           <h2>The Team</h2>
-          <div className="card mb-3">
-            <img
-              className="card-img-top"
-              src={`${assetPrefix}/static/img/header-06-faith-statement.jpg`}
-              width="80%"
-              alt="Card image cap"
-            />
-            <div className="card-body">
-              <p className="card-text">
-                <small className="text-muted">Photo caption</small>
-              </p>
-              <p className="card-text">
-                We are a diverse team working in various industries with a
-                passion for women related ministries. Our friendships and open
-                discussions regarding the struggles faced by Christians - in
-                ministry, at work and as women - fuels the desire to acknowledge
-                and support others like us out there!
-              </p>
+          <p className="">
+            We are a diverse team working in various industries with a passion
+            for women related ministries. Our friendships and open discussions
+            regarding the struggles faced by Christians - in ministry, at work
+            and as women - fuels the desire to acknowledge and support others
+            like us out there!
+          </p>
+          <div className="team">
+            <div className="row">
+              <ProfileCard
+                name="Su Heng"
+                title="Board of Directors (Sydney)"
+                image="Su.jpg"
+                description="Su is currently completing her Graduate Diploma in Divinity and has worked in various industries including non-profit, food & beverage and the arts. Her passion includes connecting God’s heart to women in the marketplace and her definition of fun is, a trip to a library and spending limitless hours there. Su leads the team in vision implementation."
+              />
+              <ProfileCard
+                name="Flora Ngai"
+                title="Board of Directors (Sydney)"
+                image="Flora.jpg"
+                description="Flora is a Marketing professional who is passionate about new technology and analytics. Sometimes, she geeks out on Digital Marketing and test runs new apps and software. For fun, Flora loves playing piano duets and making up silly tunes to sing to her 4 months old daughter."
+              />
+              <ProfileCard
+                name="Taizya Phiri"
+                title="Board of Directors (Sydney)"
+                image="Taizya.jpg"
+                description="Taizya, affectionately known as Tazi is a social worker in the NGO space and works with at risk youth. She’s passionate about social justice, inclusion and visibility of women in society. Outside of work, Taizya loves connecting with new people and brewing kombucha."
+              />
+              <ProfileCard
+                name="Joycelyn Lui"
+                title="Website Creation Team (Sydney)"
+                image="Joycelyn.jpg"
+                description="Joycelyn is a software engineer with experiences in full-stack web development, loves exploring new technologies and aspires to work on projects that deliver life-changing solutions to make a difference in the world. For fun, she spends her time playing sports such as basketball, running, hiking and indoor rock climbing."
+              />
+              <ProfileCard
+                name="Josephine Cagu"
+                title="(Sydney)"
+                image="Josephine.jpg"
+                description="Josephine has just completed her Doctor of Philosophy in Public Health and is passionate about raising cultural awareness in health service delivery to reduce disparities and promote inclusivity. She has spent over a decade in Malaysia and enjoys travelling, cooking and all things related to wellness in her spare time."
+              />
+              <ProfileCard
+                name="Mei’en Lim"
+                title="Secretary (Perth)"
+                image="Meien.jpg"
+                description="Mei’En is a clinical psychologist and also has a PhD. She works in both the government department in WA and the private sector, and sees a wide range of clients across the lifespan. Mei’En loves being close to nature, connecting with others, and word puzzles. Being married to Barry who works FIFO, she fluctuates between obsessing over her Rhodesian Ridgeback and wifedom."
+              />
+              <ProfileCard
+                name="Marissa Teo"
+                title="leads the expansion of Emboldened in Singapore (Singapore)"
+                image="Marissa.jpg"
+                description="Marissa has covered regional ASEAN roles across Business Development and Account Management in her 5.5 years at Oracle NetSuite. She is passionate about causes that seek to empower the future generation, especially involving technology. Marissa speaks 4 languages and has lived in 4 different cities!"
+              />
+              <ProfileCard
+                name="Uma Cristy"
+                title="leads the expansion of Emboldened in Kuala Lumpur (Malaysia)"
+                image="Uma.jpg"
+                description="For the past 6 years, Uma has been working in the Human Resource field placing people to the right job. She is passionate about people walking in their fullest potential that God has ordained for them. Easy-going, jovial and always the centre of laughter, Uma winds down in her spare time buried in a good book!"
+              />
             </div>
           </div>
         </div>
